e363ca587142103a693c1f13e7523b61669573e0
Lucas Moyano <lucas.95moyano@gmail.com>
2021-05-14 22:56:11 -0300

    Parametros query

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,31 +1,44 @@
 
-
 const usuariosGet = (req, res) => {
+
+    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+
     res.json({ 
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
     });
 }
 
 const usuariosPut = (req, res) => {
+
+    const id = req.params.id;
+
     res.json({ 
-        msg: 'put API - controlador'
+        msg: 'put API - controlador',
+        id
     });
 }
-
 
 const usuariosPost = (req, res) => {
+
+    const { nombre, edad } = req.body;
+
     res.json({ 
-        msg: 'post API - controlador'
+        msg: 'post API - controlador',
+        nombre, 
+        edad
     });
 }
-
 
 const usuariosDelete = (req, res) => {
     res.json({ 
         msg: 'delete API - controlador'
     });
 }
-
 
 
 module.exports = {
